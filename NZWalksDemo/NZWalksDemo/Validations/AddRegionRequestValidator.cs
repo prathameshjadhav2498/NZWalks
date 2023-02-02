86a8d411bfb641899c983f2a5b5928d141f926b5
prathameshj2498 <prathamesh.jadhav@zcomsolutions.com>
2023-02-03 01:24:39 +0530

    fluent validation added

---
+++ NZWalksDemo/NZWalksDemo/Validations/AddRegionRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using NZWalksDemo.Models.DTO;
+
+namespace NZWalksDemo.Validations
+{
+    public class AddRegionRequestValidator : AbstractValidator<AddRegionRequest>
+    {
+        public AddRegionRequestValidator()
+        {
+            RuleFor(r=>r.Code).NotEmpty().NotNull().WithMessage("{property} is required");
+            RuleFor(r=>r.Name).NotEmpty().NotNull().WithMessage("{property} is required");
+        }
+    }
+}
